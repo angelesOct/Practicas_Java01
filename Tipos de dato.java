f48698506841b47adc37cbc60c2574b86b351550
Angeles_Octaviano <37378856+angelesOct@users.noreply.github.com>
2018-05-17 22:06:26 -0500

    Update and rename Tipos de dato.md to Tipos de dato.java

--- Tipos de dato.java
+++ Tipos de dato.java
@@ -8,8 +8,7 @@ class MyClass
         System.out.println ("int \t" + Integer.MIN_VALUE );
         System.out.println ("long \t" + Long.MIN_VALUE );
         System.out.println ("float \t" + Float.MIN_VALUE );
-        System.out.println ("duble \t" + Double.MIN_VALUE + "\n");
-        
+        System.out.println ("duble \t" + Double.MIN_VALUE + "\n")
         System.out.println("******RANGO MAXIMO******");
         System.out.println ("byte \t" +Byte.MAX_VALUE);
         System.out.println ("byte \t" + Byte.MAX_VALUE );
